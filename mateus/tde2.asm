6b53ebd3abe7f2b63984710dc98be3f104c9d49b
MateusRW-br <207720@upf.br>
2025-11-12 14:53:25 -0300

    Adicionando tde2

--- mateus/tde2.asm
+++ mateus/tde2.asm
@@ -1,9 +1,37 @@
 ; Matricula: 207720    Mateus Roque Weiler
 %include '../util.asm'
 NUMERO  equ 5
+section     .text
+global      _start
 _start:
-    CMP     r15, NUMERO
-    jne     teste
+    mov     r15, NUMERO
+voltar:
+    CMP     r15, 0  ; Verifica se nao e zerio
+    je      zero
+    call    maiorMenorF
+    jmp     voltar
+maiorMenorF: ; R12 = maior R13 = menor ;
+    call    readint
+    cmp     r12, r13
+    je      inicial
+    cmp     rax, r12
+    CMOVG   r12, rax
+    cmp     rax, r13
+    CMOVL   r13, rax
+    dec     r15
+    ret
+
+inicial:
+    mov     r12, rax
+    call    readint
+    cmp     rax, r12
+    mov     r13, r12
+    CMOVG   r12, rax
+    CMOVL   r13, rax
+    dec     r15
+    dec     r15
+    ret
+zero:   
     lea     rdi, [prompt]
     call    printstr
     SUB     r12, r13
@@ -11,11 +39,6 @@ _start:
     call    printint
     call    endl
     call    exit0
-teste: ; R12 = maior R13 = menor ;
-    call    readint
-    cmp     rax, r12
-    CMOVG   r12, rax
-    CMOVL   r13, rax
-    jmp     _start
-
+    
+section     .data
 prompt:     db 'Diferença: ' , 0
